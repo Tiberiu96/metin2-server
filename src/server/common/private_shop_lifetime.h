0d7b21ccc65b602b67a6e36422a3921ad9c53e0a
Tiberiu96 <szabotiberiu96@yahoo.com>
2026-09-07 16:11:10 +0300

    update private_shop_lifetime.h

--- src/server/common/private_shop_lifetime.h
+++ src/server/common/private_shop_lifetime.h
@@ -1,7 +1,7 @@
 #pragma once
 
-// Test duration. Set to 48 * 60 * 60 for production; existing deadlines are retained.
-constexpr unsigned PRIVATE_SHOP_LIFETIME_SECONDS = 120;
+// Production duration for newly created shops: 48 hours.
+constexpr unsigned PRIVATE_SHOP_LIFETIME_SECONDS = 48 * 60 * 60;
 
 inline bool PrivateShopDeadlinePassed(long long deadline, long long now)
 {
